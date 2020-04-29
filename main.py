18530d8b6a73783dd909e96748022f1b88fae619
Riccardo De Zen <riccardo.dezen@studenti.unipd.it>
2020-04-29 20:42:40 +0200

    feat (main.py): just a test script

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import subprocess
 import pathlib
 import json
-import discord_downloader
+from src import *
 
 from pathlib import Path
-from discord_downloader import DiscordDownloader
+from src.discord_downloader import ChatDownloader
+from src.discord_parser import QuoteParser
+from src.latex_converter import *
 
 CONFIG_FILE = Path("config.json")
 
@@ -15,10 +17,20 @@ token = config["discord_token"]
 channel = config["discord_channel_id"]
 dwn_path = Path(config["download_path"])
 # Making sure download dir exists
-if not dwn_path.exists():
+if dwn_path.is_dir() and not dwn_path.exists():
     dwn_path.mkdir()
 dwn_path = dwn_path.resolve()
 
-DiscordDownloader(dce_path, token).download_channel(channel, dwn_path)
+ChatDownloader(dce_path, token).download_channel(channel, dwn_path, "json")
+
+data = json.load(dwn_path.open("r"))
+quotes = QuoteParser().parse(data, config["aliases"])
+
+grouped = group_msg_by_author(quotes)
+print(grouped)
+DocBuilder().makedoc(
+    grouped, "Le Cit-Cogne", "By Le Cicogne"
+).generate_pdf("output/output", clean_tex=False)
+
 
 print("Ay lmao my dude, done 100%.")
